6fea0af5c130640c0c96b464a068604b749c7fca
Miki Watts <mikiw@orb-software.com>
2023-10-17 22:13:47 +0300

    Basic lightgem works

---
+++ src/main.ino
@@ -0,0 +1,68 @@
+#include <arduino.h>
+
+int Led = 13;// define LED Interface
+int touchSensorValue = 0;
+int touchSensorInputPin = 3;
+int ledPin =8;
+
+long lastDebounceTime = 0;
+unsigned long debounceDelay = 200;
+int sensorState = 0;
+int lastSensorState = 0;
+
+bool isTouched;
+
+bool CurrentTouchState();
+
+void setup ()
+{
+    Serial.begin(9600);
+    pinMode(ledPin,OUTPUT);
+    pinMode (LED_BUILTIN, OUTPUT) ;//define LED as output interface
+    digitalWrite(LED_BUILTIN,LOW);
+    pinMode (touchSensorInputPin, INPUT) ;// define metal touch sensor output interface
+}
+
+void loop ()
+{
+    isTouched = CurrentTouchState();
+
+    if (isTouched)
+    {
+        analogWrite(ledPin,255);
+    }
+    else
+    {
+        analogWrite(ledPin,0);
+    }
+}
+
+bool CurrentTouchState() {
+    int currentSensorValue = digitalRead(touchSensorInputPin);
+
+    if (touchSensorValue != lastSensorState)
+    {
+        lastDebounceTime = millis();
+    }
+
+    touchSensorValue = currentSensorValue;
+
+    if (millis() - lastDebounceTime > debounceDelay) {
+        if (currentSensorValue != sensorState) {
+            sensorState = currentSensorValue;
+
+            if (sensorState == 1)
+            {
+                isTouched = true;
+            }
+            else
+            {
+                isTouched = false;
+            }
+            Serial.println(sensorState);
+        }
+    }
+
+    return isTouched;
+}
+
